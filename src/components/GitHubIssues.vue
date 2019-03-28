9324c89baa3e73e8e44ac2e8f476a439919906e7
leonardo.feitosa@universidadebrasil.edu.br <>
2019-03-28 15:15:51 -0300

    Exibindo erros

--- src/components/GitHubIssues.vue
+++ src/components/GitHubIssues.vue
@@ -3,7 +3,9 @@
     <h1>Vue.js + Github</h1>
     <p class="lead">Página que lista issues de um repositório do Github, usando Vue.js.</p>
 
-    <div class="alert alert-danger">response.message</div>
+    <div v-if="response.status === 'error'" class="alert alert-danger">
+      {{ response.message }}
+    </div>
 
     <div class="row">
       <div class="col">
@@ -51,7 +53,7 @@
         </tr>
 
         <template v-if="!loader.getIssue">
-            <tr v-if="!!issues.length && !loader.getIssues"
+            <tr v-if="showIssues"
                 v-for="issue in issues"
                 :key="issue.number">
                 <td>
@@ -67,7 +69,7 @@
                 <td>{{ issue.title }}</td>
             </tr>
         </template>
-        <tr v-if="!!!issues.length && !loader.getIssues">
+        <tr v-if="noIssues">
           <td class="text-center" colspan="2">Nenhuma issue encontrada!</td>
         </tr>
       </tbody>
@@ -80,31 +82,65 @@ import axios from 'axios';
 
 export default {
   name: 'GitHubIssues',
+  created() {
+    this.getLocalData();
+  },
   data() {
     return {
       username: '',
       repository: '',
       issues: [],
+      response: {
+        status: '',
+        message: '',
+      },
       loader: {
         getIssues: false,
         getIssue: false,
       },
     };
   },
+  computed: {
+    showIssues() {
+      return !!this.issues.length && !this.loader.getIssues;
+    },
+    noIssues() {
+      return !this.issues.length && !this.loader.getIssues;
+    },
+  },
   methods: {
     reset() {
       this.username = '';
       this.repository = '';
+      localStorage.removeItem('gitHubIssues');
+    },
+    resetResponse() {
+      this.response.status = '';
+      this.response.message = '';
     },
     getIssues() {
+      this.resetResponse();
+      this.issues = {};
       if (this.username && this.repository) {
+        localStorage.setItem('gitHubIssues', JSON.stringify({ username: this.username, repository: this.repository }));
         this.loader.getIssues = true;
         const url = `https://api.github.com/repos/${this.username}/${this.repository}/issues`;
         axios.get(url).then((response) => {
           this.issues = response.data;
+        }).catch((error) => {
+          this.response.status = 'error';
+          this.response.message = error.response.data.message;
         }).finally(() => {
           this.loader.getIssues = false;
         });
+      }
+    },
+    getLocalData() {
+      const localData = JSON.parse(localStorage.getItem('gitHubIssues'));
+      if (localData && localData.username && localData.repository) {
+        this.username = localData.username;
+        this.repository = localData.repository;
+        this.getIssues();
       }
     },
   },
